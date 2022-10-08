7f68ab719f7503c374d54fbf152cc8719d2a279e
Jonathan Vieira <jonathansouza59@gmail.br>
2022-10-07 22:03:08 -0300

    fix: launch list.

--- src/app/module/launch/launch.module.ts
+++ src/app/module/launch/launch.module.ts
@@ -1,15 +1,14 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AppMaterialModule } from 'src/app/shared/app-material/app-material.module';
+import { SharedModule } from 'src/app/shared/shared.module';
+
+import { LaunchListComponent } from './components/launch-list/launch-list.component';
+import { LaunchFormComponent } from './containers/launch-form/launch-form.component';
 import { LaunchComponent } from './containers/launch/launch.component';
 import { LaunchRoutingModule } from './launch-routing.module';
 
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatCardModule} from '@angular/material/card';
-import { AppMaterialModule } from 'src/app/shared/app-material/app-material.module';
-import { SharedModule } from 'src/app/shared/shared.module';
-import { LaunchFormComponent } from './containers/launch-form/launch-form.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { LaunchListComponent } from './components/launch-list/launch-list.component';
 
 
 @NgModule({
