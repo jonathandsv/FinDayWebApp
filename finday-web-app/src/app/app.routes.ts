060962318b55862212f33efb3995bb713e372197
Jonathan Vieira <jonathansouza59@gmail.br>
2023-11-14 11:32:09 -0300

    feat: add initial form.

--- finday-web-app/src/app/app.routes.ts
+++ finday-web-app/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LaunchComponent } from './modules/launch/launch.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: AppComponent
+    },
+    {
+        path: 'launch',
+        component: LaunchComponent
+    }
+];
---
+++ finday-web-app/src/app/modules/launch/launch.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-launch',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './launch.component.html',
+  styleUrl: './launch.component.scss'
+})
+export class LaunchComponent {
+
+}
