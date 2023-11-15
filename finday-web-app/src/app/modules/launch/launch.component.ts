dd604f12b2f75d0fa5524fc3e4ff5648204ab123
Jonathan Vieira <jonathansouza59@gmail.br>
2023-11-15 09:08:51 -0300

    feat: first controls.

--- finday-web-app/src/app/modules/launch/launch.component.ts
+++ finday-web-app/src/app/modules/launch/launch.component.ts
@@ -1,13 +1,47 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, Location } from '@angular/common';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormGroup, NgForm, NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-launch',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule, 
+    ReactiveFormsModule,
+    RouterModule
+  ],
   templateUrl: './launch.component.html',
   styleUrl: './launch.component.scss'
 })
-export class LaunchComponent {
+export class LaunchComponent implements OnInit {
+  @ViewChild('formTemplate') public formTemplate!: NgForm;
+  form!: FormGroup
 
+  constructor(
+    private fb: NonNullableFormBuilder,
+    private route: ActivatedRoute,
+    private location: Location
+    ) {}
+  
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      description: ['', [Validators.required]],
+      value: ['', [Validators.required]],
+      category: ['', [Validators.required]],
+      wallet: ['', [Validators.required]],
+      isInstallment: [false, [Validators.required]],
+      timesInstallment: [0, [Validators.required]],
+      launchDate: ['', [Validators.required]]
+    });
+  }
+
+  onSubmit(): void {
+
+  }
+
+  cleanForm(): void {
+    this.form.reset();
+    this.formTemplate.resetForm();
+  }
 }
