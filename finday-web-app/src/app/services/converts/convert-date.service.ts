3fb17cd3dc5bffa97480aa2ec21cb42bc14e29f5
Jonathan Vieira <jonathansouza59@gmail.br>
2023-11-25 14:15:47 -0300

    fix: service to convert date.

--- finday-web-app/src/app/services/converts/convert-date.service.ts
+++ finday-web-app/src/app/services/converts/convert-date.service.ts
@@ -8,11 +8,23 @@ export class ConvertDateService {
 
   constructor() { }
 
-  convertNgbDateStructToDate(value: NgbDateStruct): Date {
+  convertNgbDateStructToDate(value: NgbDateStruct | string): Date {
+    if (typeof value == 'string') {
+      // 14-11-2023
+      debugger
+      const array = value.split('-');
+      const date: NgbDateStruct = {
+        day: parseInt(array[0]),
+        month: parseInt(array[1]),
+        year: parseInt(array[2])
+      }
+      value = date
+    }
     const date = new Date(value.year, value.month - 1, value.day);
     return date;
   }
   convertDateToNgbDateStruct(value: string): NgbDateStruct {
+    debugger
     const datejs = new Date(value);
     const date: NgbDateStruct = { 
       year: datejs.getFullYear(), 
