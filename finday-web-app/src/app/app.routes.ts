899402bc1bd13d58ce2ccb839754d4eca301b5bb
Jonathan Vieira <jonathansouza59@gmail.br>
2024-02-14 20:51:29 -0300

    fix: redirect primary router.

--- finday-web-app/src/app/app.routes.ts
+++ finday-web-app/src/app/app.routes.ts
@@ -10,7 +10,7 @@ export const routes: Routes = [
     {
         path: '',
         pathMatch: 'full',
-        redirectTo: 'launchs'
+        redirectTo: 'login'
     },
     {
         path: 'launch',
